fc3f2143d35f7c04e38150864908dd8ddc9d5fc3
JeinKh <juliakhezhun@gmail.com>
2024-08-28 13:36:29 +0300

    createSelector

--- src/components/TodoList/List.jsx
+++ src/components/TodoList/List.jsx
@@ -1,11 +1,15 @@
 import { useDispatch, useSelector } from "react-redux";
-import { selectTodos } from "../../redux/todos/selectors";
+import {
+  selectSortedTodos,
+  // selectTodos,
+  selectUncompletedTodosMemo,
+} from "../../redux/todos/selectors";
 // import { deleteTodo } from "../../redux/todos/slice";
 import { selectFilter } from "../../redux/filter/selectors";
 import { deleteTodoThunk, toggleTodoThunk } from "../../redux/todos/operations";
 
 export const List = () => {
-  const todos = useSelector(selectTodos);
+  const todos = useSelector(selectSortedTodos);
   const filter = useSelector(selectFilter);
 
   const dispatch = useDispatch();
@@ -13,21 +17,26 @@ export const List = () => {
   const filteredData = todos.filter((item) =>
     item.todo.toLowerCase().includes(filter.toLowerCase())
   );
+
+  const uncompletedTodos = useSelector(selectUncompletedTodosMemo);
   return (
-    <ul>
-      {filteredData.map((item) => (
-        <li key={item.id}>
-          <input
-            type="checkbox"
-            checked={item.completed}
-            onChange={() => dispatch(toggleTodoThunk(item))}
-          />
-          <p>{item.todo}</p>
-          <button onClick={() => dispatch(deleteTodoThunk(item.id))}>
-            Delete
-          </button>
-        </li>
-      ))}
-    </ul>
+    <div>
+      <h1>Uncompleted: {uncompletedTodos}</h1>
+      <ul>
+        {filteredData.map((item) => (
+          <li key={item.id}>
+            <input
+              type="checkbox"
+              checked={item.completed}
+              onChange={() => dispatch(toggleTodoThunk(item))}
+            />
+            <p>{item.todo}</p>
+            <button onClick={() => dispatch(deleteTodoThunk(item.id))}>
+              Delete
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 };
--- src/redux/todos/slice.js
+++ src/redux/todos/slice.js
@@ -1,4 +1,4 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, isAnyOf } from "@reduxjs/toolkit";
 import {
   addTodoThunk,
   deleteTodoThunk,
@@ -10,12 +10,18 @@ const initialState = {
   items: [{ id: 1, todo: "Learn Redux" }],
   isLoading: false,
   isError: false,
+  sortType: "all",
 };
 
 // 2.
 const slice = createSlice({
   name: "todos",
   initialState,
+  reducers: {
+    changeSortType: (state, action) => {
+      state.sortType = action.payload;
+    },
+  },
   // reducers: {
   //   deleteTodo: (state, action) => {
   //     state.items = state.items.filter((item) => item.id !== action.payload);
@@ -28,17 +34,17 @@ const slice = createSlice({
     builder
       .addCase(fetchTodosThunk.fulfilled, (state, action) => {
         state.items = action.payload;
-        state.isLoading = false;
+        // state.isLoading = false;
       })
-      .addCase(fetchTodosThunk.pending, (state, action) => {
-        state.isLoading = true;
-      })
+      // .addCase(fetchTodosThunk.pending, (state, action) => {
+      //   state.isLoading = true;
+      // })
       .addCase(deleteTodoThunk.fulfilled, (state, action) => {
         state.items = state.items.filter((item) => item.id !== action.payload);
       })
-      .addCase(deleteTodoThunk.rejected, (state, action) => {
-        state.isError = true;
-      })
+      // .addCase(deleteTodoThunk.rejected, (state, action) => {
+      //   state.isError = true;
+      // })
       .addCase(addTodoThunk.fulfilled, (state, action) => {
         state.items.push(action.payload);
       })
@@ -48,15 +54,48 @@ const slice = createSlice({
             ? { ...item, completed: !item.completed }
             : item
         );
-      });
+      })
+      .addMatcher(
+        isAnyOf(
+          fetchTodosThunk.pending,
+          deleteTodoThunk.pending,
+          addTodoThunk.pending
+        ),
+        (state) => {
+          state.isLoading = true;
+          state.isError = false;
+        }
+      )
+      .addMatcher(
+        isAnyOf(
+          fetchTodosThunk.rejected,
+          deleteTodoThunk.rejected,
+          addTodoThunk.rejected
+        ),
+        (state) => {
+          state.isLoading = false;
+          state.isError = true;
+        }
+      )
+      .addMatcher(
+        isAnyOf(
+          fetchTodosThunk.fulfilled,
+          deleteTodoThunk.fulfilled,
+          addTodoThunk.fulfilled
+        ),
+        (state) => {
+          state.isLoading = false;
+        }
+      );
   },
 });
 // 3.
 export const todosReducer = slice.reducer;
+export const { changeSortType } = slice.actions;
 // export const {
 //   deleteTodo,
 //   addTodo,
 //   setErrorStatus,
-//   setLoadingStatus,
+// setLoadingStatus,
 //   fetchData,
 // } = slice.actions;
